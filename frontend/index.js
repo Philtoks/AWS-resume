23a7bce2b4a3dde0e6ba5c6ca0989443ebcf962b
Philip Itok <itokphilip@yahoo.com>
2025-03-01 03:01:56 -0800

    Upload frontend content

---
+++ frontend/index.js
@@ -0,0 +1,18 @@
+// Check if Website has been loaded, then call function for API Call
+
+window.addEventListener("DOMContentLoaded", (event)=> {
+   
+    reviseCount();
+   
+})
+
+
+// The URL of the AWS API GATEWAY 
+let AWS_API_GATEWAY_URL = 'https://w0os4gqp84.execute-api.us-east-1.amazonaws.com/getCount';
+
+/*----------API Call----------*/
+async function reviseCount() {
+    let response = await fetch(AWS_API_GATEWAY_URL);
+    let data = await response.json();
+    document.getElementById("counter").innerText = data.pageViews;
+  }
