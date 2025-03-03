5d4fc3b9e9afe4a8faa399b7553fce977d6ef92f
Philip Itok <itokphilip@yahoo.com>
2025-03-03 15:47:11 -0800

    Update JSON body key name from 'count' to 'counter'

--- backend/api/lambda_function.py
+++ backend/api/lambda_function.py
@@ -45,7 +45,7 @@ def lambda_handler(event, context):
                     }
                 }
             )
-            # JSON body to be returned count to frontend
+            # JSON body to be returned counter to frontend
             json_body = {'id': ID, 'pageViews': count}
              
         
