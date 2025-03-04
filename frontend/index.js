6619d333f1edee9a6801a929c08eebaf086b9fe6
Philip Itok <itokphilip@yahoo.com>
2025-03-03 18:12:05 -0800

    Refactor API response structure

--- frontend/index.js
+++ frontend/index.js
@@ -14,5 +14,5 @@ let AWS_API_GATEWAY_URL = 'https://w0os4gqp84.execute-api.us-east-1.amazonaws.co
 async function reviseCount() {
     let response = await fetch(AWS_API_GATEWAY_URL);
     let data = await response.json();
-    document.getElementById("counter").innerText = data.body.pageViews;
+    document.getElementById("counter").innerText = data.pageViews;
   }
