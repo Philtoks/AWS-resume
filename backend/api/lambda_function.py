4fa30e1494cc5a285cda293088b794d3c2d8d74e
Philip Itok <itokphilip@yahoo.com>
2025-03-03 17:08:50 -0800

    Refactor zip file name and update comment

--- backend/api/lambda_function.py
+++ backend/api/lambda_function.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
                 ':val1': count
             }
         )
-        # JSON body to be returned counter to frontend
+        # JSON body to be returned count to frontend
         json_body = {'id': ID, 'pageViews': count}
         
     except Exception as e:
